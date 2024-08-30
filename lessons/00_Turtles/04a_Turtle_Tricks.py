3ae7cde79cabef9c9434ca700eb63db690f773ea
jacksonvail <jacksonv@saintjamesacademy.com>
2024-08-30 19:11:47 +0000

    Today was the first day

--- lessons/00_Turtles/04a_Turtle_Tricks.py
+++ lessons/00_Turtles/04a_Turtle_Tricks.py
@@ -1,23 +1,45 @@
-"""
-For this program, you will tell Tina the Turtle to draw 
-a triangle.
+import random
+import turtle
 
-You should look at the previous program, 02_Meet_Tina.py
-to see how to use the turtle commands.
+# Returns a random color!
+def getRandomColor():
+    return "#%06X" % (random.randint(0, 0xFFFFFF))
 
 
-"""
+colors = ["red", "blue", "green", "yellow", "orange"]
 
-# These lines are needed in most turtle programs
-import turtle                           # Tell Python we want to work with the turtle
-turtle.setup (width=600, height=600)    # Set the size of the window
+def getNextColor(i):
+    return colors[i % len(colors)]
 
-tina = turtle.Turtle()                  # Create a turtle named tina
+window = turtle.Screen()
 
-# Use tina.forward() and tina.left() to draw a triangle
-# Make each side of the triangle a different color with 
-# tina.pencolor()
+baseSize = 150  # the size of the black part of the star
+flameSize = 80  # the length of the flaming arms
 
-... # Your code here
+t = turtle.Turtle()
+t.shape("turtle")
+t.width(2) 
+t.speed(0)  
 
-turtle.exitonclick()                    # Close the window when we click on it
+
+for i in range(25):
+
+    t.pencolor(getRandomColor())
+    t.fillcolor(getRandomColor())  
+
+    t.begin_fill()
+
+    t.right(360 / 8) 
+    t.forward(64)
+
+    t.left(40) 
+
+    t.forward(flameSize)
+    t.right(170) 
+    t.forward(flameSize)
+    t.right(62) 
+    t.forward(baseSize) 
+
+    t.end_fill()
+
+t.hideturtle() # Hide your turtle so you can see the pattern.
